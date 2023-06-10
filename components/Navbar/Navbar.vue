3ade8573af145f59965eb921eee16e2d0a2cb8de
SQ Langkawi <sqshadiq@SQs-MacBook-Air.local>
2023-06-10 21:04:41 +0800

    First Commit

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="Navbar">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    };
+</script>
---
+++ components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    };
+</script>
