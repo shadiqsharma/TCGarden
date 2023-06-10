c63d7cc94914c271b2cfceee3bba75b31c741e50
sqshadiq <shadiqsharma@gmail.com>
2023-06-10 21:29:32 +0800

    second push

--- components/header/header.vue
+++ components/header/header.vue
@@ -1,5 +1,5 @@
 <template xmlns="http://www.w3.org/1999/html">
-    <div >
+    <div>
       <section class="relative w-screen h-screen bg-top bg-cover md:bg-center" style="background-image:url('https://res.cloudinary.com/dgsljij3o/image/upload/v1686398741/TC/palm-tree-in-tropical-garden-2022-12-29-02-40-50-utc_1_vypze0.jpg')">
         <div class="absolute inset-0 w-full h-full bg-gray-900 opacity-25"></div>
         <div class="absolute inset-0 w-full h-64 opacity-50 bg-gradient-to-b from-black to-transparent"></div>
@@ -12,8 +12,8 @@
               <h1 class="font-extrabold tracking-tight text-center text-white text-3xl lg:text-left xl:pr-32">Welcome To</h1>
               <h1 class="font-extrabold tracking-tight text-center text-white text-7xl lg:text-left xl:pr-32">TC GARDEN RESORT</h1>
             </div>
-            <div class="w-full max-w-sm mt-20 lg:mt-0 lg:w-1/3">
-
+            <div class="flex flex-col items-center justify-center w-full h-full max-w-sm mt-20 lg:mt-0 lg:w-1/3 ">
+              <button class="text-center text-white bg-yellow-500 w-80 h-10 rounded-md"> BOOK NOW! </button>
             </div>
           </div>
         </div>
