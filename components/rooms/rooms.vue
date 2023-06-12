3ef4f13a05c74f37da86a4e63afab4811153141b
sqshadiq <shadiqsharma@gmail.com>
2023-06-12 23:54:43 +0800

    room section, shadow added

--- components/rooms/rooms.vue
+++ components/rooms/rooms.vue
@@ -2,7 +2,7 @@
     <div>
       <section class="py-10 bg-gray-50 overflow-hidden">
         <div class="container mx-auto px-4">
-          <div class="py-16 px-8 bg-white rounded-3xl">
+          <div class="py-16 px-8 bg-white rounded-3xl shadow-md">
             <div class="max-w-7xl mx-auto">
               <div class="mb-12 md:max-w-4xl mx-auto text-center">
                 <h2 class="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-gray-900 font-black tracking-tight">Our Rooms</h2>
@@ -10,7 +10,7 @@
               </div>
               <div class="flex flex-wrap -m-4 mb-6">
                 <div class="w-full md:w-1/3 p-4">
-                  <div class="h-full bg-gray-100 rounded-3xl"><img class="w-full" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
+                  <div class="h-full bg-gray-100 rounded-3xl shadow-lg"><img class="w-full rounded-3xl shadow-lg" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
                     <div class="p-8"><a class="group inline-block mb-4" href="#">
                       <h3 class="font-heading text-2xl text-gray-900 hover:text-gray-700 group-hover:underline font-black">Deluxe King Villa</h3></a>
                       <p class="text-sm text-gray-500 font-bold">RM 150.00 pernight <br/><span class="inline-block mb-4 text-xs text-orange-500 font-bold tracking-widest">Breakfast for 2 paxs included</span></p>
@@ -18,7 +18,7 @@
                   </div>
                 </div>
                 <div class="w-full md:w-1/3 p-4">
-                  <div class="h-full bg-gray-100 rounded-3xl"><img class="w-full" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
+                  <div class="h-full bg-gray-100 rounded-3xl shadow-lg"><img class="w-full rounded-3xl shadow-lg" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
                     <div class="p-8"><a class="group inline-block mb-4" href="#">
                       <h3 class="font-heading text-2xl text-gray-900 hover:text-gray-700 group-hover:underline font-black">Deluxe Triple Villa</h3></a>
                       <p class="text-sm text-gray-500 font-bold">RM 185.00 pernight <br/><span class="inline-block mb-4 text-xs text-orange-500 font-bold tracking-widest">Breakfast for 3 paxs included</span></p>
@@ -26,7 +26,7 @@
                   </div>
                 </div>
                 <div class="w-full md:w-1/3 p-4">
-                  <div class="h-full bg-gray-100 rounded-3xl"><img class="w-full" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
+                  <div class="h-full bg-gray-100 rounded-3xl shadow-lg"><img class="w-full rounded-3xl shadow-lg" src="https://res.cloudinary.com/dgsljij3o/image/upload/v1686572326/TC/a-bed-in-the-room-with-a-picture-frame-on-the-wall-2021-09-01-00-22-38-utc_v6lcce.jpg" alt="">
                     <div class="p-8"><a class="group inline-block mb-4" href="#">
                       <h3 class="font-heading text-2xl text-gray-900 hover:text-gray-700 group-hover:underline font-black">Deluxe Quad Villa</h3></a>
                       <p class="text-sm text-gray-500 font-bold">RM 220.00 pernight <br/><span class="inline-block mb-4 text-xs text-orange-500 font-bold tracking-widest">Breakfast for 4 paxs included</span></p>
