04fc2e7cc19d6f1ae96ade147273b0aaa148da1f
sqshadiq <shadiqsharma@gmail.com>
2023-06-19 22:31:18 +0800

    Deluxeking room page updated. Fixed some bugs.

--- components/rooms/rooms.vue
+++ components/rooms/rooms.vue
@@ -43,7 +43,8 @@
                 </div>
               </div>
               <div class="flex flex-wrap md:justify-center -m-2">
-                <div class="w-full md:w-auto p-2"><a class="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-gray-600 rounded-md" href="#">Book Now</a></div>
+                <div class="w-full md:w-auto p-2"><a class="block w-full px-12 py-3.5 text-lg text-center text-white font-bold bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-gray-600 rounded-md" href="https://book-directonline.com/properties/tcgardenresortlangkawidirect
+">Book Now</a></div>
               </div>
             </div>
           </div>
